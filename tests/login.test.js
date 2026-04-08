8d259909c6f8cce46a7e8eb1f57d3928d9ccaf98
Tiago <tiago.ti.tsl@mail.com>
2026-04-08 17:32:57 -0300

    Adicionando fixtures para os dados de entrada do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,14 +1,17 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
-    stages:[
+    
+    stages: [
         { duration: '10s', target: 10 },
         { duration: '20s', target: 10 },
         { duration: '10s', target: 30 },
-        { duration: '20s', target: 30},
+        { duration: '20s', target: 30 },
         { duration: '20s', target: 0 }
     ],
+
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
@@ -18,10 +21,7 @@ export const options = {
 export default function () {
     const url = 'http://localhost:3000/login';
 
-    const payload = JSON.stringify({
-    username: 'julio.lima',
-    senha: '123456',
-});
+    const payload = JSON.stringify(postLogin);
 
     const params = {
         headers: {
@@ -31,9 +31,9 @@ export default function () {
 
     const res = http.post(url, payload, params);
 
-    check (res, {
+    check(res, {
         'Validar que o Status é 200': (r) => r.status === 200,
-        'Validar que o Token é string': (r) => typeof(r.json().token) == 'string'
+        'Validar que o Token é string': (r) => typeof (r.json().token) == 'string'
     })
 
     sleep(1);
