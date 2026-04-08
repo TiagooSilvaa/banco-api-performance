a2f5ab6e28e116106416d1775ea303d86a3838a3
Tiago <tiago.ti.tsl@mail.com>
2026-04-08 14:22:49 -0300

    Adicionei VUs e Duration para execução do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,9 +2,10 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-    iterations: 50,
+    vus: 10,
+    duration: '30s',
     thresholds: {
-        http_req_duration: ['p(90)<150', 'max<200'],
+        http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
     }
 };
@@ -27,7 +28,7 @@ export default function () {
 
     check (res, {
         'Validar que o Status é 200': (r) => r.status === 200,
-        'Validar que o token é string': (r) => typeof(r.json().token) == 'string'
+        'Validar que o Token é string': (r) => typeof(r.json().token) == 'string'
     })
 
     sleep(1);
